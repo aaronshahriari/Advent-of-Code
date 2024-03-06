247e4865826ae7d2204b5ad964c350a5d5e0b7bd
Aaron Shahriari <aaronshahriari@gmail.com>
2024-03-06 08:43:28 -0500

    day 2 of aoc, wrote part 1 of color counter

--- advent_of_code_2023/day2/main.go
+++ advent_of_code_2023/day2/main.go
@@ -4,23 +4,28 @@ import (
 	"day2/lib"
 	"day2/parts"
 	"fmt"
-    "time"
+	"strings"
+	"time"
 )
 
-func part1() {
-    ///// MAIN /////
-    content := lib.ReadTxt("input.txt")
-    fmt.Println(string(content))
-
-    parts.Tester()
-    ///// MAIN /////
+func part1(content []string) int{
+    total := parts.SumColors(content)
+    return total
 }
 
 func main() {
     // starting time to check optimization
     start := time.Now()
 
-    part1()
+    content := lib.ReadTxt("inputs/sample_p1.txt")
+    // content := lib.ReadTxt("inputs/input.txt")
+    split_content := strings.Split(string(content), "\n")
+
+    ///// PART 1 /////
+    total := part1(split_content)
+    fmt.Println("Total:", total)
+
+    ///// PART 2 /////
 
     // elapsed time for optimization
     stop := time.Now()
--- advent_of_code_2023/day2/parts/part1.go
+++ advent_of_code_2023/day2/parts/part1.go
@@ -1,7 +1,69 @@
 package parts
 
 import (
+	"fmt"
+	"strings"
+    "strconv"
+    "log"
 )
 
-func Tester() {
+// CONSTRAINTS:
+// > 12 RED
+// > 13 GREEN
+// > 14 BLUE
+// RESULT: ADD GAME NUMBER UP
+func SumColors(content []string) int{
+    id_sum := 0
+
+    red_constr := 12
+    green_constr := 13
+    blue_constr := 14
+
+    for game := range content {
+        game_message := fmt.Sprintf("Game %v: ", game + 1)
+        content[game] = strings.TrimPrefix(content[game], game_message)
+        content[game] = strings.ReplaceAll(content[game], ",", "")
+        sep_content := strings.Split(content[game], ";")
+
+        for sep := range sep_content {
+            single_it_map := make(map[string]string)
+            parts := strings.Fields(sep_content[sep])
+
+            // iterate over the slices
+            // starting from index 1 and incrementing by 2
+            for i := 1; i < len(parts); i += 2 {
+                single_it_map[parts[i]] = parts[i-1]
+            }
+
+            // gets the value of the map
+            // returns 0 if it does not exist
+            getValue := func(key string) int{
+                if value, ok := single_it_map[key]; ok {
+                    value, err := strconv.Atoi(value)
+                    if err != nil {
+                        log.Fatal(err)
+                    }
+                    return value
+                }
+                return 0
+            }
+
+            fmt.Println(single_it_map)
+
+            // check if current map is not 0 then continue
+            if len(single_it_map) == 0 {
+            } else {
+                red_count := getValue("red")
+                green_count := getValue("green")
+                blue_count := getValue("blue")
+                if (red_count < red_constr &&
+                green_count < green_constr &&
+                blue_count < blue_constr) {
+                    fmt.Println("CHECKS OUT")
+                    continue
+                } else { fmt.Println("DOESN'T CHECKS OUT") }
+            }
+        }
+    }
+    return id_sum
 }
